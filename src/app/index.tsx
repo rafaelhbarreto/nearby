ca1bcea3f5225881ffe564b935ab6eebea9284f4
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-12-10 17:23:00 -0300

    feat: add router to home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from 'react-native'
 import { Welcome } from '@/components/Welcome'
 import { Steps } from '@/components/Steps'
+import { Button } from '@/components/Button'
+import { router } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40}}>
       <Welcome />
       <Steps />
-      
+      <Button onPress={ () => router.navigate("/home")}>
+        <Button.title>
+          Começar
+        </Button.title>
+      </Button>
     </View>
   )
 }
