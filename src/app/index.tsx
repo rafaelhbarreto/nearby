1ef5ce9cccccb848f21c6f0648201bc246f6f8a0
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-12-09 21:53:47 -0300

    feat: add start component application

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { Welcome } from '@/components/Welcome'
+import { Steps } from '@/components/Steps'
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40}}>
+      <Welcome />
+      <Steps />
+      
+    </View>
+  )
+}
