32fdba1ba22c567ca49083b1133fedd9b1a8ce20
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-12-10 17:55:22 -0300

    feat: configure axios

--- app/src/app/home.tsx
+++ app/src/app/home.tsx
@@ -1,6 +1,23 @@
-import {View, Text} from 'react-native'
+import { useEffect } from 'react'
+import {View, Text, Alert} from 'react-native'
+import { api } from '@/config/axios'
 
 export default function Home() {
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+      Alert.alert("Erro ao buscar categorias")  
+    } 
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
   return (
     <View>
       <Text>
