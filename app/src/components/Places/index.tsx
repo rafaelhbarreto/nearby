119d26026684c1f25606ab131e2d50d655eb5c1b
Rafael Barreto <rafaelhbarreto@gmail.com>
2024-12-12 13:28:39 -0300

    feat: finish bottomsheet

--- app/src/components/Places/index.tsx
+++ app/src/components/Places/index.tsx
@@ -1,8 +1,8 @@
 import React, {useRef} from "react";
-import { useWindowDimensions } from 'react-native'
-import { Place, PlaceProps } from '../Place'
+import { Text, useWindowDimensions } from 'react-native'
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
-
+import { Place, PlaceProps } from '../Place'
+import { s } from "./styles";
 
 type Props = {
   data: PlaceProps[]
@@ -13,12 +13,26 @@ export function Places({ data }: Props) {
   const dimensions = useWindowDimensions()
   const bottomSheetRef = useRef<BottomSheet>(null)
   
+  const snapPoints = {
+    min: 278,
+    max: dimensions.height - 128
+  }
+
   return (
-      <BottomSheet>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[snapPoints.min, snapPoints.max]}
+        handleIndicatorStyle={s.indicator}
+        backgroundStyle={s.container}
+        enableOverDrag={false}
+      >
         <BottomSheetFlatList 
           data={data}
           keyExtractor={item => item.id}
           renderItem={({ item }) => <Place data={item} />}
+          contentContainerStyle={s.content}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={() => <Text>Explore lugares perto de você!</Text>}
         />
 
       </BottomSheet>
